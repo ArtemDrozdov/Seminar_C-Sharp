a1f8c43611655097f7f8b9588d44baf25680f772
ArtemDrozdov <drozdov_as@mail.ru>
2023-02-11 01:46:37 +0300

    HW_S9

---
+++ HW_S9_002/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+Console.Write("Введите M: --> ");
+int minRange = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите N: --> ");
+int maxRange = Convert.ToInt32(Console.ReadLine());
+
+int SumNumbersMN(int m = 1, int n = 15)
+{
+    if (m == n)
+        return n;
+    else
+        return n + SumNumbersMN(m, --n);
+}
+
+Console.WriteLine($"сумма натуральных элементов в промежутке от {minRange} до {maxRange} = " + SumNumbersMN(minRange, maxRange));
---
+++ HW_S9_003/Program.cs
@@ -0,0 +1,40 @@
+﻿/*
+Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+m = 2, n = 3 -> A(m,n) = 9
+m = 3, n = 2 -> A(m,n) = 29
+
+https://ru.wikipedia.org/wiki/%D0%A4%D1%83%D0%BD%D0%BA%D1%86%D0%B8%D1%8F_%D0%90%D0%BA%D0%BA%D0%B5%D1%80%D0%BC%D0%B0%D0%BD%D0%B0
+в таблице поменяны M и N
+*/
+Console.Write("Введите M: --> ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите N: --> ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("ack = ");
+Console.WriteLine(ack(m, n));
+Console.Write("ack2 = ");
+Console.WriteLine(ack2(m, n));
+
+int ack(int n, int m)
+{
+    if (n == 0)
+        return m + 1;
+    else if (m == 0)
+        return ack(n - 1, 1);
+
+    return ack(n - 1, ack(n, m - 1));
+}
+
+int ack2(int n, int m)
+{
+    while (n != 0)
+    {
+        if (m == 0)
+            m = 1;
+        else
+            m = ack2(n, m - 1);
+        n = n - 1;
+    }
+    return m + 1;
+}
